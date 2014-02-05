2a3e57a7a07a72fffaab5fb31308211e0aeaf9fb
Jonathan Zung <jonathanzung@gmail.com>
2014-02-05 18:44:37 -0500

    -

--- capture.cpp
+++ capture.cpp
@@ -1,14 +1,12 @@
+#ifdef __clang__
+typedef struct { long double x, y; } __float128;
+#endif
+
 #include "OpenNI.h"
 #include <PS1080.h>
 #include <iostream>
 #include <string>
 #include <math.h>
-#include <complex>
-
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/range_image/range_image_planar.h>
-#include <pcl/filters/filter.h>
 
 #include <map>
 #include <fstream>
@@ -16,79 +14,20 @@
 #include <ctype.h>
 #include <unistd.h>
 
+#include <Eigen/Core>
+#include "util.h"
+#include <thread>
+
 using namespace openni;
 using namespace std;
-using namespace pcl;
 
-struct parameter
-{
-	int i;
-	float f;
-	string s;
-};
+bool a = true;
+typedef Eigen::Matrix<short unsigned int, Eigen::Dynamic, Eigen::Dynamic> MatrixXsi;
 
-map<string, parameter> loadConfig(const char* filename)
+void wait()
 {
-	ifstream f;
-	f.open(filename);
-	string t;
-	string param;
-	string val;
-	map<string, parameter> config;
-	while(getline(f,t,' '))
-	{
-		getline(f,param,'=');
-		getline(f,val);
-		boost::algorithm::trim(param);
-		parameter x;
-		if(t=="int")
-		{
-			x.i= atoi(val.c_str());
-		}
-		else if(t == "string")
-		{
-			x.s=val;
-		}
-		else if(t=="float")
-		{
-			x.f=atof(val.c_str());
-		}
-		config[param]=x;
-	}
-	return config;
-}
-PointCloud<PointXYZRGB> toPCD(const void* a, unsigned char* b, int width, int height, float xfov, float yfov, float cutoff)
-{
-	RangeImagePlanar range_im;
-	PointCloud<PointXYZRGB> cloud;
-	range_im.setDepthImage(((const short unsigned int*) a), width, height, width/2, height/2, (float)(width/2)/tan(xfov/2), (float)(height/2)/tan(yfov/2));
-	cloud.width = height*width;
-	cloud.height = 1;
-	cloud.points.resize(height*width);
-	cloud.is_dense=false;
-	int pointcount = 0;
-	for(int i = 0; i < width; i++)
-	{
-		for(int j = 0; j < height; j++)
-		{
-			if(range_im.at(i,j).range < cutoff && range_im.at(i,j).x != 0)
-			{
-				cloud.points[pointcount].x = range_im.at(i,j).x;
-				cloud.points[pointcount].y = range_im.at(i,j).y;
-				cloud.points[pointcount].z = range_im.at(i,j).z;
-				cloud.points[pointcount].r = b[3*(i+j*width)];
-				cloud.points[pointcount].g = b[3*(i+j*width)+1];
-				cloud.points[pointcount].b = b[3*(i+j*width)+2];
-				pointcount++;
-			}
-		}
-	}
-	cloud.points.resize(pointcount);
-	vector<int> indices;
-	removeNaNFromPointCloud(cloud,cloud,indices);
-	cloud.width = cloud.points.size();
-
-	return cloud; 
+	cin.ignore();
+	a=false;
 }
 
 int main(int argc, char** argv)
@@ -98,8 +37,7 @@ int main(int argc, char** argv)
 	string output = params["output"].s;
 	int delay = params["delay"].i;
 	int num_frames = params["num_frames"].i;
-	float cutoff = params["cutoff"].f;
-	
+
 	int c;
 	while((c = getopt (argc, argv, "o:n:")) !=-1)
 	{
@@ -113,44 +51,30 @@ int main(int argc, char** argv)
 				break;
 		}
 	}
-	int mode_id = 5;
-	int mode_id_RGB = 9;
-	string folder = output;
-	
+	int mode_id = 1;
+
 	vector<void*> data;
-	vector<void*> dataRGB;
 	OpenNI::initialize();
 	Device* cam = new Device();
-	
 	Array<DeviceInfo>* deviceInfoList = new Array<DeviceInfo>;
 	OpenNI::enumerateDevices(deviceInfoList);
 	cam->open(deviceInfoList->operator[](0).getUri());
-
 	VideoStream* vid = new VideoStream();
 	vid->create(*cam, SENSOR_DEPTH);
 
-	VideoStream* vidRGB = new VideoStream();
-	vidRGB->create(*cam, SENSOR_COLOR);	
-
 	VideoMode mode;
-	VideoMode modeRGB;
 	mode = vid->getSensorInfo().getSupportedVideoModes()[mode_id];
-	modeRGB = vidRGB->getSensorInfo().getSupportedVideoModes()[mode_id_RGB];
 	int width = mode.getResolutionX();
 	int height = mode.getResolutionY();
-
 	cam->setImageRegistrationMode(IMAGE_REGISTRATION_DEPTH_TO_COLOR);
 	cam->setDepthColorSyncEnabled(true);
-	
+
 	cout << "Registration Mode: " << cam->getImageRegistrationMode() << endl;
 	cout << "Depth Pixel Format: " << mode.getPixelFormat() << endl;
 	cout << "Resolution: " << mode.getResolutionX() << "x" << mode.getResolutionY() << "x" << mode.getFps() << endl;
-	cout << "RGB Pixel Format: " << modeRGB.getPixelFormat() << endl;
-	cout << "RGBResolution: " << modeRGB.getResolutionX() << "x" << modeRGB.getResolutionY() << "x" << modeRGB.getFps() << endl;
 	cout << "Field of View: " << vid->getHorizontalFieldOfView() << "x" << vid->getVerticalFieldOfView() << endl;
 
 	vid->setVideoMode(mode);
-	vidRGB->setVideoMode(modeRGB);
 
 	float xfov = vid->getHorizontalFieldOfView();
 	float yfov = vid->getVerticalFieldOfView();
@@ -162,38 +86,40 @@ int main(int argc, char** argv)
 	int datasize = frame->getDataSize();
 	cout << "Depth Datasize: " << datasize << endl;
 	frame->release();
-
-	vidRGB->start();
-
-	VideoFrameRef* frameRGB = new VideoFrameRef();
-	vidRGB->readFrame(frameRGB);
-	int datasizeRGB = frameRGB->getDataSize();
-	cout << "RGB Datasize: " << datasizeRGB <<endl;
-	frameRGB->release();
-
 	cin.ignore();
 
-	for(int i = 0; i<num_frames; i++)
+	thread t1(wait);	
+	for(int i = 0; i<num_frames && a; i++)
 	{
 		data.push_back(new void* [datasize]);
-		dataRGB.push_back(new void*[datasizeRGB]);
-		//cout << "Frame: " << i << endl;
+		cout << "." << flush;
+		if(i%10 == 9)
+		{
+			cout << endl;
+		}
 		vid->readFrame(frame);
-		vidRGB->readFrame(frameRGB);
 		memcpy(data[i], frame->getData(), datasize);
-		memcpy(dataRGB[i], frameRGB->getData(), datasizeRGB);
 		frame->release();
-		frameRGB->release();
-		
 		usleep(delay*1000);
 	}
+	t1.join();
 	vid->stop();
-	for(int i = 0; i < data.size(); i++)
+	OpenNI::shutdown();
+
+	vector<Eigen::MatrixXf> dataXf;
+	for(int i=0; i<data.size(); i++)
 	{
-		PointCloud<PointXYZRGB> cloud = toPCD(data[i],(unsigned char*)dataRGB[i],width,height,xfov,yfov, cutoff);
-		stringstream filename;
-		filename << folder << i << ".pcd";
-		io::savePCDFileBinary(filename.str(), cloud);
-		cout << "saved cloud of " << cloud.points.size() << " to " << filename.str()  << endl;
+		Eigen::Map<MatrixXsi> m((short unsigned int*)(data[i]), width, height);
+		Eigen::MatrixXf n(width, height);
+		for(int j=0; j<width; j++)
+		{
+			for(int k=0; k<height; k++)
+			{
+				n(j,k)=(float)(m(j,k));
+			}
+		}
+		dataXf.push_back(n);
 	}
+
+	eigenToFile(dataXf, output);
 }
---
+++ view.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import sys
+from tvtk.api import tvtk
+from mayavi import mlab
+
+
+filename = sys.argv[1]
+stlr = tvtk.STLReader()
+stlr.file_name = file_name = filename
+stlr.update()
+stld = stlr.output
+
+fig = mlab.figure(bgcolor=(1,1,1), fgcolor=(0,0,0))
+surf = mlab.pipeline.surface(stld, opacity=1, color=(0.5,0.5,0.5))
+#edge = mlab.pipeline.extract_edges(surf)
+#edge_surf = mlab.pipeline.surface(edge,opacity=.1, color=(1,0,0))
+mlab.show()
